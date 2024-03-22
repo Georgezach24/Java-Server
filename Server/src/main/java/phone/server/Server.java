85589bf4910951582e76c4c807ff9df5f8fd2c1d
george <giorgoszaxos2003@gmail.com>
2024-03-22 14:54:11 +0200

    First commit

---
+++ Server/src/main/java/phone/server/Server.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package phone.server;
+
+/**
+ *
+ * @author giorg
+ */
+public class Server {
+
+    public static void main(String[] args) {
+        
+    }
+}
