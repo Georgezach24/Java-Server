c5025a514ec85c34afe3c85f0f8e6d6e29ce7eec
george <giorgoszaxos2003@gmail.com>
2024-03-23 12:25:00 +0200

    some server stracture

---
+++ Server/src/main/java/phone/server/Contact.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package phone.server;
+
+/**
+ *
+ * @author Γεώργιος Ζάχος (ΑΜ:321/2021020)
+ */
+public class Contact {
+
+    private String name, surname, number;
+
+    public Contact(String name, String surname, String number) {
+        this.name = name;
+        this.surname = surname;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+    
+    
+
+}
