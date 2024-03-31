99db138a95bd861ae2d2a1b6b0a516705046b350
george <giorgoszaxos2003@gmail.com>
2024-03-31 12:13:52 +0300

    Client ready and comented

--- Client/src/main/java/MainWindow.java
+++ Client/src/main/java/MainWindow.java
@@ -187,7 +187,7 @@ public class MainWindow extends javax.swing.JFrame {
                 outstream.flush();
                 
                 //Ανάγνωση και εμφάνηση του αποτελέσματος της αναζήτησης.
-                String in = instream.readUTF();
+                String in = instream.readObject().toString();
                 JOptionPane.showMessageDialog(this, in, "Search Result", JOptionPane.INFORMATION_MESSAGE);
                 
                 //Ανάγνωση και εμφάνηση της κατάστασης του server.
